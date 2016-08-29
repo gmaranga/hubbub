1f0821f7854e493c24b9b1828200c2c2e967b9d7
gmm <gustavo.maranga@gmail.com>
2016-08-29 17:47:26 -0300

    User search feature

--- grails-app/controllers/com/grailsinaction/UserController.groovy
+++ grails-app/controllers/com/grailsinaction/UserController.groovy
@@ -4,4 +4,14 @@ class UserController {
 
     static scaffold = User
 
+    def search(){}
+
+    def results(String loginId){
+
+        def users = User.where{
+            loginId =~ "%${loginId}%"
+        }.list()
+
+        return [users: users, term: params.loginId, totalUsers: User.count()]
+    }
 }
