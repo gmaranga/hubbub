14a998a9798b776e659362390b7d1a3f010111f9
gmm <gustavo.maranga@gmail.com>
2016-08-29 15:04:05 -0300

    Initial commit

---
+++ grails-app/domain/com/grailsinaction/User.groovy
@@ -0,0 +1,25 @@
+package com.grailsinaction
+
+class User {
+
+	String loginId
+	String password
+	Date dateCreated
+	
+	static hasOne = [profile: Profile]
+	static hasMany = [posts: Post, tags: Tag, following: User]
+	
+    static constraints = {
+		loginId size: 3..20, unique: true, nullable: false
+		password size: 6..8, nullable: false, validator: {passwd, user -> passwd != user.loginId}
+		tags()
+		posts()
+		profile nullable: true
+    }
+
+	String toString(){
+		return "User ${loginId} (id: $id)"
+	}
+
+	
+}
